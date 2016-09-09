0f0f6ced7f8a299e407d9f9666cc78c85fd92afd
Gunnar Wolf <gwolf@gwolf.org>
2016-09-09 16:36:33 -0500

    Ejemplo de Makefile desarrollado en clase

---
+++ ejemplos_en_clase/makefile-01/func_calc.c
@@ -0,0 +1,19 @@
+
+
+int suma (int a, int b) {
+    return a + b;
+}
+
+int resta (int a, int b) {
+    return a - b;
+}
+
+int mult (int a, int b) {
+    return a * b;
+}
+
+int div (int a, int b) {
+    if (b != 0)
+    	return a / b;
+    return -1;
+}
