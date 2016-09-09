21322b4312cf8bd1e6aa8fcb5e86d57bdbd4512e
mauricio <mauricio.quijada@outlook.com>
2016-09-09 17:43:10 -0500

    Agregue mi Carpeta MauricioQJ

---
+++ practicas/09.09/Mau_EjercicioMake/calc.h
@@ -0,0 +1,9 @@
+/* calculadora de pila inversa: calc.h */
+
+#define NUMERO '0'	/* señal para detección de número */
+void push(double);	/* extrae de la pila */
+double pop(void);	/* coloca en la pila */
+int getop(char[]);
+int getch(void);
+void ungetch(int);
+
