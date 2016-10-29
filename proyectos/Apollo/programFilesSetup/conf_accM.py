70c55e5d146c7804b20df32b988fe60dc8a333bf
Ezzzzzzzzzzzzzz <e_rojas@live.com>
2016-10-29 01:04:25 -0500

    Agrego comentarios y documentacion del proyecto

--- proyectos/Apollo/programFilesSetup/conf_accM.py
+++ proyectos/Apollo/programFilesSetup/conf_accM.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import smbus
 
-# Get I2C bus
-bus = smbus.SMBus(1)
 
-# MMA8452Q address, 0x1C(28)
-# Select Control register, 0x2A(42)
-#		0x00(00)	StandBy mode
-bus.write_byte_data(0x1D, 0x2A, 0x00)
-# MMA8452Q address, 0x1C(28)
-# Select Control register, 0x2A(42)
-#		0x01(01)	Active mode
-bus.write_byte_data(0x1D, 0x2A, 0x01)
-# MMA8452Q address, 0x1C(28)
-# Select Configuration register, 0x0E(14)
-#		0x00(00)	Set range to +/- 2g
+bus = smbus.SMBus(1)										#OBTENEMOS BUS DE PROTOCOLO I2C 
+
+bus.write_byte_data(0x1D, 0x2A, 0x00)		# MMA8452Q CON DIRECCION DE IDENTIFICACION, 0x1D
+																				# SELECT CONTROL REGISTER, 0x2A(42)
+																				#		0x00(00)	STANDBY MODE
+
+bus.write_byte_data(0x1D, 0x2A, 0x01)		
+																				# SELECT CONTROL REGISTER , 0x2A(42)
+																				#		0x01(01)	ACTIVE MODE
 bus.write_byte_data(0x1D, 0x0E, 0x00)
+																				# SELECT CONFIGURATION REGISTER, 0x0E(14)
+																				#		0x00(00)	SET RANGE +/- 2g
 
 
